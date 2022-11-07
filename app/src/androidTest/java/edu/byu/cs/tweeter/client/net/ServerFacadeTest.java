78b5e37528cade4620807b9e06fb12faa619a83f
Noah Pratt <prattnj@gmail.com>
2022-11-07 16:09:21 -0700

    m3 done, tests included

--- app/src/androidTest/java/edu/byu/cs/tweeter/client/net/ServerFacadeTest.java
+++ app/src/androidTest/java/edu/byu/cs/tweeter/client/net/ServerFacadeTest.java
@@ -1,7 +1,6 @@
 package edu.byu.cs.tweeter.client.net;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
